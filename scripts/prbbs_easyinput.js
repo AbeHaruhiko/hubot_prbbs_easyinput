974888227928e2ba02319ab5edc743847aafa147
AbeHaruhiko <caliconography@gmail.com>
2017-12-26 05:42:49 +0000

    botが結果をつぶやくよう修正

--- scripts/prbbs_easyinput.js
+++ scripts/prbbs_easyinput.js
@@ -7,16 +7,27 @@
 const easyinput = require('prbbs_easyinput');
 
 module.exports = (robot) => {
-  robot.respond(/start (.+)( )(.+)/i, (msg) => {
-    const user = msg.match[1].trim();
-    const pass = msg.match[3].trim();
-    const result = easyinput.easyInput(user, pass, 'start');
-    msg.send(result);
-});
-robot.respond(/end (.+)( )(.+)/i, (msg) => {
-  const user = msg.match[1].trim();
-  const pass = msg.match[3].trim();
-  const result = easyinput.easyInput(user, pass, 'end');
-  msg.send(result);
-});
+  robot.respond(/start (.+)/i, (msg) => {
+    const paramArray = msg.match[1].split(' ');
+    const user = paramArray[0];
+    const pass = paramArray[1];
+    const time = paramArray.length >= 3 ? paramArray[2] : null;
+    const promise = easyinput.easyInput(user, pass, 'start', time);
+    promise.then((result) => {
+      console.log(result);
+      msg.send(result);
+    });
+  });
+  robot.respond(/end (.+)/i, (msg) => {
+    const paramArray = msg.match[1].split(' ');
+    console.log(paramArray);
+    const user = paramArray[0];
+    const pass = paramArray[1];
+    const time = paramArray.length >= 3 ? paramArray[2] : null;
+    const promise = easyinput.easyInput(user, pass, 'end', time);
+    promise.then((result) => {
+      console.log(result);
+      msg.send('出勤時刻: ' + result.intime + ', 退勤時刻: ' + result.outtime);
+    });
+  });
 };
